0836534b27837a611845b8c8d5cba8ed636cd4b7
mackudos <mackudos@gmail.com>
2015-12-26 15:42:24 +0300

    initial commit

---
+++ voltMobiTask/source/backend/Common/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  voltMobiTask
+//
+//  Created by Sergey on 26/12/15.
+//  Copyright © 2015 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
